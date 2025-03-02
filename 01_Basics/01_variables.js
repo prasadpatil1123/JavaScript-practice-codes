e6bc95647a933416ea03845a9efc589335292431
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-02 18:36:21 +0000

    lecture_05

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const country = "India";
+let fullname = "kashi";
+var city = " Pune";
+college = "sinhgad Institute";
+
+let company;
+// name = "prasad"
+
+/*city = "Mumbai"
+console.log(city);*/
+console.table([country, fullname, city, college, company]);
+
+// console.log(name);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3+3)  // we are using nodejs , not browser 
+
+console.log(3+3)
+
+let myname = "Kashi"
+let age = 10
+let isLogged = true
+let state ;
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// null -> Object
+// undefined 
+// symbol -> unique
+
+
+// Object
+
+console.log(typeof String)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof Number)
+console.log(typeof BigInt)
+console.log(typeof Symbol)
