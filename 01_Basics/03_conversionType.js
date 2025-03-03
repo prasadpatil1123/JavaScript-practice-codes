7f910b414ba26634c1b8344dc7eaf50d99d97f6e
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-03 06:06:34 +0000

    conversion

---
+++ 01_Basics/03_conversionType.js
@@ -0,0 +1,35 @@
+let score = 33
+// let score = "123abc"
+// let score = "abc"
+
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+// let isLog = "Kashi"
+// let isLog = true
+// let isLog = 1
+// let isLog = 0
+let isLog = ""
+
+// let booleanIsLog = Boolean(isLog)
+// console.log(booleanIsLog)
+// 1 => true; 0 => false
+// "" => false
+// "Kashi" => true
+
+// let randomNum = 45
+// let randomNum = ""
+
+
+let result = String(randomNum)
+console.log(result)
+console.log(typeof result)
