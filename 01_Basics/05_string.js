81879d7af43a517acaf22f94a7007613bdbe7501
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-11 09:24:31 +0000

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,40 @@
+const name = "Kashi"
+const repoCount = 53
+
+// console.log(name + repoCount+" Value")
+// console.log(`My name is ${name}. My Repository counts ${repoCount} on github.`)
+
+const myString = "Kashiprasad"
+
+// console.log(myString.length) //1111111111
+// console.log(myString.at(1))//22222222222222222222222
+// console.log(myString.at(0))
+// console.log(myString.charAt(5)) //3333333333
+// console.log(myString.indexOf('r')) //7777777777
+
+// const astr = myString.substring(2,9) //88888888888
+// const astr = myString.toLocaleLowerCase()
+// const astr = myString.toLocaleUpperCase()
+const astr = myString.slice(1,5)
+// console.log(astr)
+
+const school = new String("CT-Bora-College")
+
+// console.log(school.__proto__) //4444444444444444
+// console.log(school[3]) //5555555555555
+
+const engineer = "    Mechanical Engineer        "
+// console.log(engineer.trim()) //6666666666666
+
+const url = `http://localhost:3000/kashi%30prasad`
+
+// console.log(url.includes("react"))
+// console.log(url.concat("react"))
+// console.log(url.split())
+// console.log(url.replace('%30','_'))
+
+
+
+
+
+
