2dd39c0ffaec3624d17eb76f8f8d67aaf8e77008
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-11 10:08:43 +0000

    nums and Math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score)
+
+const marks = new Number(150)
+// console.log(marks)
+
+const balance = new Number(10000000)
+// console.log(balance)
+// console.log(balance.toFixed(2))
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toLocaleString())
+// console.log(balance.toLocaleString('en-In'))
+
+const otherNumber = 125.6575
+// console.log(otherNumber.toPrecision(4))
+
+// +++++++++++ Math +++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(123.56))
+// console.log(Math.round(123.36))
+// console.log(Math.ceil(4.4))
+// console.log(Math.ceil(123.1))
+// console.log(Math.floor(123.56))
+
+// console.log(Math.min(25,46,98,15))
+// console.log(Math.max(25,46,98,15))
+// console.log(Math.random())
+// console.log(Math.random() * 10 +1)
+
+const min = 10
+const max = 20
+
+console.log((Math.random()*(max-min+1))+1)
+// console.log(Math.floor((Math.random()*(max-min+1))+1))
