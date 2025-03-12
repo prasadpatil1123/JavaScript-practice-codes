d9ed78c17663c81f0540c574ae69b27ba928c9bf
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-12 17:49:40 +0000

    date

---
+++ 01_Basics/07_dateinjs.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log("myDate.toString()---------> "+myDate.toString());
+// console.log("myDate.toDateString()--------->"+myDate.toDateString());
+// console.log("(myDate.toISOString()------->"+myDate.toISOString());
+// console.log("myDate.toJSON()--------------->"+myDate.toJSON());
+// console.log("myDate.toLocaleDateString()----->"+myDate.toLocaleDateString());
+// console.log("myDate.toTimeString()--------->"+myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2022, 0, 6)
+// let myCreatedDate = new Date(2017, 11, 6)
+// let myCreatedDate = new Date(2017, 11, 6, 5,8)
+// console.log(myCreatedDate);
+// let myCreatedDate = new Date("2023-05-14")
+// console.log(myCreatedDate.toLocaleString());
