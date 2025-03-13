7db15ee0c04daabebf9b5c9f06a83b62bc23961e
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-13 10:11:36 +0000

    arr1

---
+++ 02_Basics/array_02.js
@@ -0,0 +1,39 @@
+let arr1 = [11,12,15,17,18]
+let myHeroes = ["Ironman", "Batman","Thor","Superman"]
+
+let arr2 = new Array(4,5,6,8)
+
+// console.log(arr2);
+// console.log(arr1[2]);
+// console.log(myHeroes[0]);
+// arr1.push(25)
+// arr1.push(28)
+// console.log(arr1);
+// arr1.pop()
+// arr1.unshift(45)
+// arr1.shift
+// console.log(arr1); // [11, 12, 15, 17, 18, 25, 28 ]
+// console.log(arr1.includes(65)); //false
+// console.log(arr1.indexOf(15)); // 2
+
+const newArr = arr1.join()
+
+// console.log(newArr);
+// console.log(arr1);
+
+// console.log("A ", arr1);
+
+// const myA1 = arr1.slice(1,4)
+// console.log(myA1);
+
+console.log("B ", arr1);
+
+const myA2 = arr1.splice(1,4)
+console.log("C ", arr1);
+console.log(myA2);
+
+
+
+
+
+
