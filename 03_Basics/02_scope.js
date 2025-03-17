3f53d3f1c4a8c4ec5ca87dbcf80594101280b23b
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-17 17:45:26 +0000

    scope

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,23 @@
+// var c = 300
+// let a = 100
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+// var c = 300
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//     const username = "Kashi"
+//     function two() {
+//         // const website = "Youtube"
+//         console.log(username);
+//     }
+//     two()
+// }
+
+// one()
+
