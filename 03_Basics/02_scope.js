87028d69b1426f8c357bee8279058492924b2879
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-17 18:18:51 +0000

    next_scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -13,11 +13,22 @@ if(true){
 // function one() {
 //     const username = "Kashi"
 //     function two() {
-//         // const website = "Youtube"
+//         const website = "Youtube"
 //         console.log(username);
 //     }
+//     // console.log(website);
 //     two()
 // }
 
 // one()
 
+if (true) {
+    const username = "Prasad"
+    if(username === "Prasad"){
+        const website = "Youtube channel "
+        console.log(`My name is ${username} I have ${website}`);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
