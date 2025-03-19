1d2014000c2cd32eaae458038b2794ef014c8942
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-19 17:45:18 +0000

    ifelsecondition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+// const isUserLoggedIn = true
+
+// if (true) {
+//     console.log(`User is logged in`);
+// }
+
+// const temp = 42
+// if (temp < 42) {
+//     console.log("Temp is less than 42 degree");
+
+// } else if (temp > 42) {
+//     console.log("Temp is greater than 42 degree");
+// } else {
+//     console.log("Temp is 42 degree");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+// let score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`The power is ${power}`);
+// }
+// console.log(`The power is ${power}`);
+
+const isUserLoggedIn = true
+const debitcard = true
+
+if(isUserLoggedIn && debitcard ){
+    console.log("You can buy course");
+}else{
+    console.log("You can't buy the course");
+}
+
+const fromGoogle = true
+const fromApple = false
+
+if (fromGoogle || fromApple) {
+    console.log("Welcome");
+}
