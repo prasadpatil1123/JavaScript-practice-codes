7a748e21ab8f172530d05cd2420a1ba9173f3209
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-19 17:48:52 +0000

    switch_condition

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+month = 2
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("APR");
+        break;
+
+    default:
+        console.log("No month");
+        
+        break;
+}
