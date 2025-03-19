366e2cbc731c90382eac97fb1a8ef6229e1459f8
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-03-19 18:08:52 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,52 @@
+// True values
+// let usermail = []
+// let usermail = "0"
+// let usermail = " "
+// let usermail = 'false'
+// let usermail = true
+// let usermail = function(){}
+// let usermail = {}
+
+
+// False values
+// let usermail = false
+// let usermail = ""
+// let usermail = NaN
+// let usermail = 0
+// let usermail = -0
+// let usermail = undefined
+// let usermail = null
+
+// if(usermail){
+//     console.log("You can access");
+// }else{
+//     console.log("You can't access");
+// }
+
+// let usermail = []
+// if (usermail.length === 0) {
+//     console.log("Success");
+// }
+
+// const emptyObj = {}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("block is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+ 
+// let value
+// value = 5 ?? 10    //5
+// value = null ?? 10   //10
+// value = undefined ?? 35  // 35
+// console.log(value);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const coffee = 55
+
+if (coffee < 50 ? console.log("less than 50 "):console.log("more than 50"));
+
+
