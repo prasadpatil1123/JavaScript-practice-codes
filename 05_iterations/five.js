893b9dde066d708219ee8e1cd15bc6d208552260
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-04-04 13:39:05 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["js", "py", "cpp"]
+
+coding.forEach(function x(val) {
+    // console.log(val);
+})
+
+coding.forEach((item) => {
+    // console.log(item);
+})
+
+function printMe(item) {
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+coding.forEach((item, index, arr) => {
+    // console.log(item,index,arr);
+
+})
+
+const myCode = [
+    {
+        name: "JavaScript",
+        type: "js"
+    },
+    {
+        name: "Python",
+        type: "py"
+    },
+    {
+        name: "Java",
+        type: "java"
+    }
+]
+
+myCode.forEach((item) => {
+    console.log(item.name);
+}
+)
