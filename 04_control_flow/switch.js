8527d0c30435e36b597c9f85b88ef00315c2cb83
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-04-04 06:45:51 +0000

    switch_again

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,21 +1,43 @@
-month = 2
+// const month = 2
 
-switch (month) {
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("APR");
+//         break;
+
+//     default:
+//         console.log("No month");
+        
+//         break;
+// }
+
+const season = 2
+
+switch (season) {
     case 1:
-        console.log("Jan");
+        console.log("It's Summar");
+        
         break;
     case 2:
-        console.log("Feb");
+        console.log("It's Mansoon");
+        
         break;
     case 3:
-        console.log("Mar");
-        break;
-    case 4:
-        console.log("APR");
+        console.log("It's Winter");
+        
         break;
 
     default:
-        console.log("No month");
+        console.log("No season");
         
         break;
 }
