60f9f85536fe7b04853c5b46780ad84ff9e57a0b
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-04-08 14:53:16 +0000

    one revision

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -5,10 +5,17 @@ map.set('I','India')
 map.set('C','China')
 map.set('S','South Africa')
 
-// for (const key in map) {
-//     console.log(key);
-// }
+for (const key in map) {
+    // console.log(key);  // No output
+}
 // console.log(map);
+// Map(5) {
+//     'B' => 'Brazil',
+//     'R' => 'Russia',
+//     'I' => 'India',
+//     'C' => 'China',
+//     'S' => 'South Africa'
+//   }
 
 const obj = {
     js:'javaScript',
@@ -26,9 +33,9 @@ for (const key in obj) {
 
 
 
-// for (const [key,value] in obj) {
+for (const [key,value] in obj) {
 // console.log(key,value);
-// }
+}
 
 // Printed:
 // js
@@ -43,6 +50,10 @@ for (const key in obj) {
 // console.log(`${key} have ${value}`);
     // console.log(`${key} shortcut is for ${obj[key]}`);
 }
+// Output:
+// js shortcut is for javaScript
+// java shortcut is for Java
+// cpp shortcut is for C++
 
 
 const myObject = {
@@ -56,10 +67,77 @@ for (const key in myObject) {
     // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+// Output : 
+// js shortcut is for javascript
+// cpp shortcut is for C++
+// rb shortcut is for ruby
+// swift shortcut is for swift by apple
+
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
-    console.log(programming[key]);
+    // console.log(programming[key]);
 }
 
+// output:
+// js
+// rb
+// py
+// java
+// cpp
+
+for( let index = 0; index <= 10; index++){
+    const element = index;
+    if (element == 5) {
+        // console.log("5 is best number");
+        break;
+        // continue;
+    }
+    // console.log(element);
+}
+
+for (let i = 1; i <= 5; i++) {
+    // const element = array[i];
+    // console.log(`Outer loop value : ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`inner loop value J : ${j} and inner loop value of I : ${i} `);   
+        // console.log(i+"*"+j+"="+ i*j);             
+    }
+}
+
+let myArray = ['JavaScript','Python','MySQL']
+
+// console.log(myArray.length); // 3
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+// output:
+// JavaScript
+// Python
+// MySQL
+
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
