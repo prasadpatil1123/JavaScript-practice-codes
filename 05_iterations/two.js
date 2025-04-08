e2c2f45ccbcb47fd321ba5072dd2755b4ff8afcb
Kashiprasad <122286888+prasadpatil1123@users.noreply.github.com>
2025-04-08 15:01:18 +0000

    two revision

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`The value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['Daredevil', 'SPiderman', 'Hulk']
+
+let arr = 0
+while(arr < myArray.length){
+    // console.log(`the value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do{
+    console.log(`the score is ${score}`); // the score is 11
+    score++;
+}while(score <= 10)
